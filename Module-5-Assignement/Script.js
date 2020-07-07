371dbc93ca35e7fd0e1fa9ae32aa49c6e55b1ae6
unknown <aakulbarkakoty99@gmail.com>
2020-07-08 00:34:54 +0530

    Ayyan

--- Module-5-Assignement/Script.js
+++ Module-5-Assignement/Script.js
@@ -1,5 +1,22 @@
-function sayHello () {
- console.log(
-  document.getElementById("name").value
-  );
-}
+// Event Handling
+document.addEventListener("DOMContentLoaded", 
+  function (event) {
+
+    // Unobstructive event binding
+    document.querySelector("button")
+      .addEventListener("Click", function () {
+        
+
+      //Call server to get the name
+      $ajaxUtils
+        .sendGetRequest("name.txt", function (request) {
+          var name = request.responseText;
+
+          document.querySelector("#content").
+          innerHTML = "<h2>Hello " + name + "!";
+        });
+
+      
+      });
+  }
+);
