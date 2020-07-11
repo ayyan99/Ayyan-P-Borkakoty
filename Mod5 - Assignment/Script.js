7b29c35fca7311f4774564d1fb68dc9842676a44
unknown <aakulbarkakoty99@gmail.com>
2020-07-12 00:22:48 +0530

    Ayyan

--- Mod5 - Assignment/Script.js
+++ Mod5 - Assignment/Script.js
@@ -8,3 +8,35 @@ $(function () {
 		}
 	});
 }); 
+
+(function (global) {
+
+var dc = {};
+
+var homeHtml = "home-snippet.html";
+
+var insertHtml = function (selector,html) {
+	var targetElem = document.querySelector(selector);
+	targetElem.innerHTML = html;
+};
+
+ar showLoading = function (selector) {
+	var html = "<div class='text-center'>";
+	html += "<img src='ajax-loader.gif'></div>";
+	insertHtml(selector, html);
+};
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+showLoading("#main-content");
+$ajaxUtils.sendGetRequest(
+	homeHtml,
+	function (responseText) {
+		document.querySelector("#main-content")
+			.innerHTML = responseText;
+	},
+	false);
+});
+
+global.$dc = dc;
+})(window);
