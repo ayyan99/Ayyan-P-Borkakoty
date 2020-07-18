f1e65ac2f7bed94bbec12802ce6894afe3199bf5
unknown <aakulbarkakoty99@gmail.com>
2020-07-18 19:27:10 +0530

    Ayyan

--- Module5-Solution/Script.js
+++ Module5-Solution/Script.js
@@ -63,7 +63,6 @@ var switchMenuToActive = function () {
 // On page load (before images or CSS)
 document.addEventListener("DOMContentLoaded", function (event) {
 
-
 // TODO: STEP 0: Look over the code from
 // *** start ***
 // to
@@ -93,18 +92,23 @@ $ajaxUtils.sendGetRequest(
 // Builds HTML for the home page based on categories array
 // returned from the server.
 function buildAndShowHomeHTML (categories) {
-
-  // Load home snippet page
+  
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
     function (homeHtml) {
+      var chosenCategoryShortName = chooseRandomCategory (categories).short_name;
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
+      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+    },
+    false); 
+}
+
+
 
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-       var chosenCategoryShortName = function chooseRandomCategory (categories) {
-        
-       }
+      // var chosenCategoryShortName = ....
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -126,9 +130,8 @@ function buildAndShowHomeHTML (categories) {
       // of how to do that.
       // ....
 
-    },
-    false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
-}
+     // False here because we are getting just regular HTML from the server, so no need to process JSON.
+
 
 
 // Given array of category objects, returns a random category object.
