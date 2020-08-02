4750215abfd09c5e39f85d88c066563600d3db70
unknown <aakulbarkakoty99@gmail.com>
2020-08-02 18:06:14 +0530

    Ayyan

--- Forms/javas.js
+++ Forms/javas.js
@@ -1,35 +1,20 @@
-/*Name this external file gallery.js*/
-
-function upDate(previewPic){
-/*  In this function you should 
-    1) change the url for the background image of the div with the id = "image" 
-    to the source file of the preview image
- 
-*/
-
-var x = document.getElementById("image");
-x.innerHTML = previewPic.alt;
-x.style.backgroundImage = "url('"+previewPic.src+"')";
-
-} 
-    /*2) Change the text  of the div with the id = "image" 
-    to the alt text of the preview image */
-
-	
-
-	function unDo(){
-     /* In this function you should 
-    1) Update the url for the background image of the div with the id = "image" 
-    back to the orginal-image.  You can use the css code to see what that original URL was
-    */
-    var x = document.getElementById("image")
-    x.innerHTML = "Hover over an image below to display here.";
-    x.style.backgroundImage = "url('')";
+function check(){
+    var email1 = document.getElementById('email1');
+    var email2 = document.getElementById('email2');
+    if (email1.value != email2.value){
+        alert("The two emails doesn't match!");
+        return false;
     }
-    /*
-    2) Change the text  of the div with the id = "image" 
-    back to the original text.  You can use the html code to see what that original text was
-    
-	
-    */
+}
 
+function nicknames(){
+    if(document.getElementById('yesnick').checked){
+        document.getElementById('nick').style.display="inline";
+        document.getElementById('nicks').setAttribute('required',true);
+    }
+    
+    else{
+        document.getElementById('nicks').removeAttribute('required');
+        document.getElementById('nick').style.display="none";
+    }
+}
