5dda4bc161a216b059e83833d851008dde3b2ba3
unknown <aakulbarkakoty99@gmail.com>
2020-08-24 20:48:25 +0530

    Ayyan

---
+++ Background/javas.js
@@ -0,0 +1,29 @@
+function red(){
+var body = document.getElementById("bodyId");
+body.style.backgroundColor = 'red';
+} 
+
+function green(){
+var body = document.getElementById("bodyId");
+body.style.backgroundColor = 'green';
+} 
+
+function yellow(){
+var body = document.getElementById("bodyId");
+body.style.backgroundColor = 'yellow';
+} 
+
+function orange(){
+var body = document.getElementById("bodyId");
+body.style.backgroundColor = 'orange';
+} 
+
+function blue(){
+var body = document.getElementById("bodyId");
+body.style.backgroundColor = 'blue';
+} 
+
+function grey(){
+var body = document.getElementById("bodyId");
+body.style.backgroundColor = 'grey';
+} 
